54fc1846fa6780b31d8699d2fa0e4830ecf04ffd
Vinskky <naitsabesdv@gmail.com>
2020-10-14 23:10:59 +0200

    NavMesh + Patrol
    AI labtest for assignment 1

---
+++ AI Behaviours/Assets/Scripts/Patrol.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class Patrol : MonoBehaviour
+{
+    private Transform[] waypoints;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //create a new array with name tagg in order, so we can do the path as we want.
+
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("WayPoint").OrderBy(go => go.name).ToArray();
+        
+        //Set transform vector = length of the sorted array
+
+        waypoints = new Transform[gos.Length];
+
+         for(int i = 0; i < gos.Length; i++)
+         {
+            //Assign transform of the vector of gameobjects sets as waypoints to the vector of transform.
+             waypoints[i] = gos[i].transform;
+         }
+
+        agent.autoBraking = false;
+
+        GoNextWayPoit();
+    }
+
+    void GoNextWayPoit()
+    {
+        if (waypoints.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = waypoints[destPoint].position;
+
+        destPoint = (destPoint + 1) % waypoints.Length;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            GoNextWayPoit();
+    }
+
+  
+}
---
+++ AI Behaviours/Assets/Scripts/Seek.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Seek : MonoBehaviour
+{
+    public GameObject target;
+    float angle;
+    float maxVelocity = 0.25f;
+    public float turnSpeed;
+    Quaternion rotation;
+    Vector3 movement;
+    Vector3 direction;
+    float freq = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        freq += Time.deltaTime;
+        if(freq > 0.5)
+        {
+            freq -= 5;
+            direction = target.transform.position - transform.position;
+            direction.y = 0f;
+
+            movement = direction.normalized * maxVelocity;
+
+            angle = Mathf.Rad2Deg * Mathf.Atan2(movement.x, movement.z);
+            rotation = Quaternion.AngleAxis(angle, Vector3.up); // up = y
+            //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
+            //transform.position = transform.forward.normalized * maxVelocity * Time.deltaTime;
+        }
+    }
+}
