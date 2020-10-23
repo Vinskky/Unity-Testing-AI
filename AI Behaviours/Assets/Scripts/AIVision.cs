ed62c22f882d102b33cf7769c9011305fac2a2ab
Vinskky <naitsabesdv@gmail.com>
2020-10-23 17:29:40 +0200

    AIVISION

---
+++ AI Behaviours/Assets/Scripts/AIVision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIVision : MonoBehaviour
+{
+    public Camera frustum;
+    public LayerMask mask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
